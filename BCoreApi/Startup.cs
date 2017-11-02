3fdbe7a30d80a81e809719c29f35cf4bf250ebbc
gagarinbefree <gagarinbefree@gmail.com>
2017-11-02 22:30:38 +0500

    tune identityserver4

--- BCoreApi/Startup.cs
+++ BCoreApi/Startup.cs
@@ -14,7 +14,6 @@ using BCoreDao;
 using BCoreDal;
 using BCoreDal.SqlServer;
 using IdentityServer4.AccessTokenValidation;
-
 namespace BCoreApi
 {
     public class Startup
@@ -54,6 +53,15 @@ namespace BCoreApi
 
             // Add unit of work 
             services.AddScoped<IUoW, SqlServerUnit>();
+
+            services.AddAuthentication(
+               IdentityServerAuthenticationDefaults.JwtAuthenticationScheme)
+                    .AddIdentityServerAuthentication(options =>
+                    {
+                        options.Authority = "http://localhost:5000"; // Auth Server
+                        options.RequireHttpsMetadata = false; // only for development
+                        options.ApiName = "fiver_auth_api"; // API Resource Id
+                    });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -64,12 +72,14 @@ namespace BCoreApi
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
-            app.UseIdentityServerAuthentication(new IdentityServerAuthenticationOptions
-            {
-                Authority = "http://localhost:5000",
-                RequireHttpsMetadata = false,
-                ApiName = "BCoreIdentity"
-            });
+            app.UseIdentityServer();
+
+            //app.UseIdentityServerAuthentication(new IdentityServerAuthenticationOptions
+            //{
+            //    Authority = "http://localhost:5000",
+            //    RequireHttpsMetadata = false,
+            //    ApiName = "BCoreIdentity"
+            //});
 
             app.UseMvc();            
         }
